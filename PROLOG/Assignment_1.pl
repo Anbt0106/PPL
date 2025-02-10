b903ea16ddda469b0b5f3f0999cfd2c19c6aaa20
Anbt <buithoan_t67@hus.edu.vn>
2025-02-10 19:59:54 +0700

    Assign 1

---
+++ PROLOG/Assignment_1.pl
@@ -0,0 +1,36 @@
+
+female(sophia).
+female(sandra).
+female(mia).
+female(lisa).
+male(peter).
+male(alex).
+male(dick).
+male(bob).
+male(andrew).
+
+parent(bob, lisa).
+parent(bob, alex).
+parent(bob, sophia).
+parent(mia, lisa).
+parent(mia, alex).
+parent(mia, sophia).
+parent(peter, andrew).
+parent(lisa, andrew).
+parent(sophia, dick).
+parent(sophia, sandra).
+
+% Rule to define father relationship
+
+
+% Rule to define sister relationship
+
+
+% Rule to define grandmother relationship
+
+
+% Rule to define wife-husband relationship
+
+
+% Rule to define cousin relationship
+
